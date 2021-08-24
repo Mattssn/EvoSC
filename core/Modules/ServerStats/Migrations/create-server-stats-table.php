9f69e2fcd1051d3b6f6b3154f8be8477e9013b86
Skordy <sk8ordiejohn@hotmail.com>
2021-08-24 11:11:38 -0700

    Updated ServerStats

--- core/Modules/ServerStats/Migrations/create-server-stats-table.php
+++ core/Modules/ServerStats/Migrations/create-server-stats-table.php
@@ -24,6 +24,13 @@ class CreateServerStatsTable extends Migration
             $table->integer('CurrentPlayers')->default(0);
             $table->string('CurrentMapName')->default("None");
         });
+
+        DB::table('server-stats')insert([
+            'Title' => "PhenexTech",
+            'MaxPlayers' => "0",
+            'CurrentPlayers' => "0",
+            'CurrentMapName' => "None",
+        ]);
     }
 
     /**
